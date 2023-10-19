eafd0728ce51e29c0788f475f8f2324880c16401
boriskisiov <boriskisiov@gmail.com>
2023-10-19 10:04:25 +0300

    Add project files.

---
+++ Sum N Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Sum_N_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            for (int i = 0; i < n; i += 1) {
+            
+                sum += double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
